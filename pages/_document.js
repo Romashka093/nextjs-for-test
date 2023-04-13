2f5f48c08276ffd189de3914fa7f62da3f51d378
Romashka093 <aa.romashchenko@gmail.com>
2023-04-13 09:20:06 +0300

    accessibility and seo fixes

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang={'en'}>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="CV of best React Frontend Developer ever" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
